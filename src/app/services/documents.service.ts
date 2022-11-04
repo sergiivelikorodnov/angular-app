6ca24c903c62a1207c520a9d085294256fddb17d
Sergii <git-autoconfig>
2022-11-04 12:36:00 +0100

    add: crud methods

--- src/app/services/documents.service.ts
+++ src/app/services/documents.service.ts
@@ -1,42 +1,35 @@
-import { HttpErrorResponse } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { CollectionReference, DocumentData, collection } from '@firebase/firestore'
-import { collectionData, Firestore } from '@angular/fire/firestore'
-import { catchError, Observable, throwError } from 'rxjs'
+import { addDoc, collectionData, deleteDoc, doc, Firestore, updateDoc } from '@angular/fire/firestore'
+import { Observable } from 'rxjs'
 import { IClient } from '../models/client'
-import { ErrorService } from './error.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentsService {
   private documentsCollection: CollectionReference<DocumentData>
-  constructor(private errorService: ErrorService, private readonly firestore: Firestore) {
+  constructor(private readonly firestore: Firestore) {
     this.documentsCollection = collection(this.firestore, 'documents')
-  }
-
-  private errorHandler(error: HttpErrorResponse) {
-    this.errorService.handler(error.message)
-    return throwError(() => error.message)
   }
 
   getAll() {
     return collectionData(this.documentsCollection, {
-      idField: 'id',
-    }) as Observable<IClient[]>;
+      idField: 'id'
+    }) as Observable<IClient[]>
   }
 
+  create(document: IClient) {
+    return addDoc(this.documentsCollection, document)
+  }
 
-  /* getData(): Observable<IClient[]> {
-    const dbInstance = collection(this.firestore, 'documents')
+  update(document: IClient) {
+    const documentReference = doc(this.firestore, `documents/${document.id}`)
+    return updateDoc(documentReference, { ...document })
+  }
 
-    getDocs(dbInstance).then(res => {
-      this.data = [
-        ...res.docs.map(item => {
-          return { ...(item.data() as IClient), id: item.id }
-        })
-      ]
-    })
-    catchError(this.errorHandler.bind(this))
-  } */
+  delete(id: string) {
+    const documentReference = doc(this.firestore, `documents/${id}`)
+    return deleteDoc(documentReference)
+  }
 }
